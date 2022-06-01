21f4f2abd28b6b5c39efb3c0e6d93a64de265037
FSahid <78040719+FFZahra@users.noreply.github.com>
2022-06-02 00:59:30 +0100

    first task attempts - for review

---
+++ task/js/api.js
@@ -0,0 +1,12 @@
+$('#resultBox').html("This is where results get displayed");
+
+$('#btn1').click(function() {
+    // alert("Okay, which country do you want to find neighbours for?");
+    $('#form1').show();
+    $('#form1btn').click(function() {
+            // alert($('#cntry').val());
+            $.ajax({
+                
+            })
+        })
+})
---
+++ task/php/getOceans.php
@@ -0,0 +1,11 @@
+<?php
+    // retrieve ocean info
+
+    ini_set('display_errors', 'On');
+    error_reporting(E_ALL);
+
+    $url=''.$_REQUEST[''].'&username=fatimahS&style=full';
+
+
+
+?>
