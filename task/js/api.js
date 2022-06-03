3408bc56bbc979b8f19fdf1d69b06499b1e69bd1
FSahid <78040719+FFZahra@users.noreply.github.com>
2022-06-03 12:18:09 +0100

    Restructuring. Second attempt at API data retrieval

--- task/js/api.js
+++ task/js/api.js
@@ -1,12 +1,41 @@
 $('#resultBox').html("This is where results get displayed");
 
-$('#btn1').click(function() {
-    // alert("Okay, which country do you want to find neighbours for?");
-    $('#form1').show();
-    $('#form1btn').click(function() {
-            // alert($('#cntry').val());
-            $.ajax({
-                
-            })
-        })
+
+$('#form1btn').click(function() {
+    /*alert($('#cntrycd').val());
+    alert($('#radius').val());
+    alert($('#postcd').val());*/
+    $.ajax({
+        url: "../php/postal.php",
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            postalcode: $('#postcd').val(),
+            country: $('#cntrycd').val(),
+            radius: $('#radius').val()
+        },
+        success: function(result) {
+            console.log(result);
+
+            if (result.status.name == "ok") {
+                $('resultBox').html(result['data'][0]['placeName'] + ' ' + result['data'][0]['postalCode'] + ' ' + result['data'][0]['lat'] + ' ' + result['data'][0]['lng']);
+            }
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            $('resultBox').html("Sorry, no data available for this request.");
+        }
+        
+    });
+});
+
+$('#form2btn').click(function() {
+    $.ajax({
+        
+    })
+})
+
+$('#form3btn').click(function() {
+    $.ajax({
+        
+    })
 })
