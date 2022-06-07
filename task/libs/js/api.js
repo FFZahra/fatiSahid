1ae3d1e359b64ea9de9dfa848a750b1fa34a18df
FSahid <78040719+FFZahra@users.noreply.github.com>
2022-06-07 09:05:04 +0100

    merging all htmls to one index.html, creating libs folder

---
+++ task/libs/js/api.js
@@ -0,0 +1,101 @@
+$('#answerArea').html("This is where results get displayed");
+
+$(document).ready(function(){
+    $("#btn1").click(function(){
+        $("#api2").hide();
+        $("#api3").hide();
+        $("#api1").show();
+        $("#cntrycd").focus();
+    })
+})
+$('#form1btn').click(function() {
+    $.ajax({url: "../php/getPostals.php",
+    type: 'POST',
+    dataType: 'json',
+    data: {
+        postalcode: $('#postcd').val(),
+        country: $('#cntrycd').val(),
+        radius: $('#radius').val()
+    },
+    success: function(result) {
+        console.log(result);
+
+        if (result.status.name == "ok") {
+            const innerList = result["postalCodes"][0];
+            $('#answerArea').html(function() {
+                `Placenames\t\tPostal Codes\n ${innerList.filter(i, placeName, postalCode => innerList[i]["placeName"] + "\t\t" + innerList[i]["postalCode"] + "\n")}`;
+            });             
+        }
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+        $('#answerArea').html("Sorry, no data available for this request.");
+    }
+    
+    })
+})
+
+$(document).ready(function(){
+    $("#btn2").click(function(){
+        $("#api1").hide();
+        $("#api3").hide();
+        $("#api2").show();
+        $("#addr").focus();
+    })
+})
+$('#form2btn').click(function() {
+    $.ajax({url: "../php/getLocation.php",
+    type: 'POST',
+    dataType: 'json',
+    data: {
+        q: $("#addr").val()
+    },
+    success: function(result) {
+        console.log(result);
+    
+        if (result.status.name == "ok") {
+            const cntryCodesArr = {"AT": 'Austria', "AU": 'Australia', "AX": 'Aland Islands', "CC": 'Cocos Islands', "CH": 'Switzerland', "CX": 'Christmas Island', "CZ": 'Czech Republic', "DK": 'Denmark', "EE": 'Estonia', "ES": 'Spain', "FI": 'Finland', "FR": 'France', "GF": 'French Guiana', "GP": 'Guadeloupe', "IS": 'Iceland', "LU": 'Luxembourg', "MQ": 'Martinique', "NF": 'Norfolk Island', "NL": 'Netherlands', "NO": 'Norway', "PL": 'Poland', "PR": 'Puerto Rico', "RE": 'Reunion', "SG": 'Singapore', "SI": 'Slovenia', "SJ": 'Svalbard and Jan Mayen', "SK": 'Slovakia', "US": 'United States', "YT": 'Mayotte'
+    };
+            $('#answerArea').html(`For address: ${result["address"]["houseNumber"]} ${result["address"]["street"]}, ${result["address"]["locality"]}, ${result["address"]["postalcode"]}\n Latitude is: ${result["address"]["lat"]}\n Longitude is: ${result["address"]["lng"]}\n Country: ${cntryCodesArr[result["address"]["countryCode"]]}.}`);            
+        };
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+        $('#answerArea').html("Sorry, no data available for this request.");
+    }
+ 
+    })
+})
+
+
+$(document).ready(function(){
+    $("#btn3").click(function(){
+        $("#api1").hide();
+        $("#api2").hide();
+        $("#api3").show();
+        $("#latd").focus();
+    })
+})
+$('#form3btn').click(function() {      
+    $.ajax({
+        url: "../php/getAddress.php",
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            lat: $('#latd').val(),
+            lng: $('#longtd').val()
+        },
+        success: function(result) {
+            console.log(result);
+            
+            rsltArr = result["address"];
+            if (result.status.name == "ok") {
+                $('#answerArea').html(`The nearest address is: ${rsltArr["houseNumber"]} ${rsltArr["street"]}, ${rsltArr["locality"]}, ${rsltArr["postalCode"]}, ${rsltArr["countryCode"]}.`);                
+            }
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            $('answerArea').html("Sorry, no data available for this request.");
+        }
+        
+    });
+});
+
+
--- task/libs/php/getLocation.php
+++ task/libs/php/getLocation.php
@@ -1,10 +1,12 @@
 <?php
-    // retrieve country neighbour info
+    // Lookup latitude and longitude data:
 
     ini_set('display_errors', 'On');
     error_reporting(E_ALL);
 
-    $url='http://api.geonames.org/neighboursJSON?formatted=true&cntry='.$_REQUEST['cntry'].'&username=fatimahS&style=full';
+    $executionStartTime = microtime(true);
+
+    $url = "http://api.geonames.org/geoCodeAddressJSON?q=".$_REQUEST['q']."&username=fatimahS";
 
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -13,11 +15,15 @@
 
     $result = curl_exec($ch);
 
-    $infoArray = json_decode($result, true);
+    curl_close($ch);
+
+    $decode = json_decode($result, true);
 
     $output['status']['code'] = "200";
     $output['status']['name'] = "ok";
-    $output['data'] = $infoArray['geonames'];
+    $output['status']['description'] = "success";
+    $output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000)." ms";
+    $output['data'] = [$decode]['address'];
 
     header('Content-Type: application/json; charset=UTF-8');
 
